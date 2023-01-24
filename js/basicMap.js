693075f05c4c8d4a02ac238d3065d23c7492d3c7
sophuang <yiqi.huang.19@ucl.ac.uk>
2023-01-24 22:17:35 +0000

    debug

--- js/basicMap.js
+++ js/basicMap.js
@@ -2,8 +2,6 @@
 
 
 let mymap; // global variable to store the map
-
-
         
 let popup = L.popup(); // create a custom popup as a global variable 
 
@@ -21,13 +19,36 @@ function onMapClick(e) {
 
 
 function loadLeafletMap() {
-                    console.log
+    mymap = L.map('mapid').setView([51.500149, -0.126240], 13); 
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>' }).addTo(mymap);
+                    console.log('load leaflet map');
 
                     // now add the click event detector to the map 
                     mymap.on('click', onMapClick);
 
+                    console.log("check on map click")
+
                     // now call the code to add the markers 
                     addBasicMarkers();
+
+                    // add a circle
+    L.circle([51.500149, -0.126240], 5000, {
+    color: 'green',
+    fillColor: '#f03',
+    fillOpacity: 0.8 }).addTo(mymap).bindPopup("I am a circle.");
+    console.log("added a circle");
+    
+    
+    // add a polygon
+    let myPolygon = L.polygon([
+    [51.709, -0.10], 
+    [51.703, 0.07], 
+    [51.22, 0.07], 
+    [51.22, -0.057] ],
+    {color: 'orange', fillColor: '#f03', fillOpacity: 0.5}).addTo(mymap).bindPopup("I am a polygon in 2022.");
+    
 } //end code to add the leaflet map
 
 
@@ -36,7 +57,7 @@ function loadLeafletMap() {
     
 
 function addBasicMarkers() {
-                    console.log
+                    console.log('add basic markers');
 
                     let testMarkerPink = L.AwesomeMarkers.icon({
                                     icon:'play',
@@ -66,23 +87,5 @@ function addBasicMarkers() {
 
 
 
-mymap = L.map('mapid').setView([51.500149, -0.126240], 13); 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-maxZoom: 19,
-attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>' }).addTo(mymap);
-
-// add a circle
-L.circle([51.500149, -0.126240], 5000, {
-color: 'green',
-fillColor: '#f03',
-fillOpacity: 0.8 }).addTo(mymap).bindPopup("I am a circle.");
-console.log("added a circle");
 
 
-// add a polygon
-let myPolygon = L.polygon([
-[51.709, -0.10], 
-[51.703, 0.07], 
-[51.22, 0.07], 
-[51.22, -0.057] ],
-{color: 'orange', fillColor: '#f03', fillOpacity: 0.5}).addTo(mymap).bindPopup("I am a polygon in 2022.");
