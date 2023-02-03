ab150f9787c5d46a4365ea87588a84cb8c70aec0
sophuang <yiqi.huang.19@ucl.ac.uk>
2023-02-03 16:50:10 +0000

    Add menu11 and menu 14

--- README.md
+++
@@ -1,2 +0,0 @@
-# cege0043-app-template
-Template repository for the app (front end) code for CEGE0043
---
+++ js/menu.js
@@ -0,0 +1,44 @@
+"use strict";
+
+function menu1() {
+    alert("menu1 clicked");
+}
+
+function menu3() {
+    alert("menu3");
+}
+
+function menu4() {
+    alert("menu4");
+}
+
+function menu6() {
+    alert("menu6");
+}
+
+function menu7() {
+    alert("menu7");
+}
+
+function menu9() {
+    alert("menu9");
+}
+
+function menu10() {
+    alert("menu10");
+}
+
+function menu11() {
+    alert("menu number is 11 and menu is called by: "+ arguments.callee.name);
+}
+
+function menu12() {
+    alert("menu number is 12 and menu is called by: "+ arguments.callee.name);
+}
+
+function menu14() {
+    alert("menu number is 14 and menu is called by: "+ arguments.callee.name);
+}
+function menu15() {
+    alert("menu number is 15 and menu is called by: "+ arguments.callee.name);
+}
