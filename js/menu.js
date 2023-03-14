b0b9592e8bf5bc99a697d90a5e4c31c5f649307e
sophuang <yiqi.huang.19@ucl.ac.uk>
2023-03-14 12:19:40 +0000

    Creating menu items for assign5 in bootstrap html

--- js/menu.js
+++ js/menu.js
@@ -1,107 +1,72 @@
 "use strict";
 
-function menu1() {
+function AssetBest() {
     let re = /([^(]+)@|at ([^(]+) \(/g;
     let aRegexResult = re.exec(new Error().stack);
     let sCallerName = aRegexResult[1] || aRegexResult[2];
-    alert("menu number is 1 and menu is called by: "+ sCallerName);
+    alert("menu AssetBest is called by: "+ sCallerName);
 }
 
-function menu3() {
+function RatesGraph() {
     let re = /([^(]+)@|at ([^(]+) \(/g;
     let aRegexResult = re.exec(new Error().stack);
     let sCallerName = aRegexResult[1] || aRegexResult[2];
-    alert("menu number is 3 and menu is called by: "+ sCallerName);
+    alert("menu RatesGraph is called by: "+ sCallerName);
 }
 
-function menu4() {
+function help() {
     let re = /([^(]+)@|at ([^(]+) \(/g;
     let aRegexResult = re.exec(new Error().stack);
     let sCallerName = aRegexResult[1] || aRegexResult[2];
-    alert("menu number is 4 and menu is called by: "+ sCallerName);
+    alert("menu help is called by: "+ sCallerName);
 }
 
-function menu6() {
+function UserRanking() {
     let re = /([^(]+)@|at ([^(]+) \(/g;
     let aRegexResult = re.exec(new Error().stack);
     let sCallerName = aRegexResult[1] || aRegexResult[2];
-    alert("menu number is 6 and menu is called by: "+ sCallerName);
+    alert("menu UserRanking is called by: "+ sCallerName);
 }
 
-function menu7() {
+function Add5assets() {
     let re = /([^(]+)@|at ([^(]+) \(/g;
     let aRegexResult = re.exec(new Error().stack);
     let sCallerName = aRegexResult[1] || aRegexResult[2];
-    alert("menu number is 7 and menu is called by: "+ sCallerName);
+    alert("menu Add5assets is called by: "+ sCallerName);
 }
 
-function menu9() {
+function Remove5assets() {
     let re = /([^(]+)@|at ([^(]+) \(/g;
     let aRegexResult = re.exec(new Error().stack);
     let sCallerName = aRegexResult[1] || aRegexResult[2];
-    alert("menu number is 9 and menu is called by: "+ sCallerName);
+    alert("menu Remove5assets is called by: "+ sCallerName);
 }
 
-function menu10() {
+function Add5reports() {
     let re = /([^(]+)@|at ([^(]+) \(/g;
     let aRegexResult = re.exec(new Error().stack);
     let sCallerName = aRegexResult[1] || aRegexResult[2];
-    alert("menu number is 10 and menu is called by: "+ sCallerName);
+    alert("menu Add5reports is called by: "+ sCallerName);
 }
 
-function menu12() {
+function Remove5reports() {
     let re = /([^(]+)@|at ([^(]+) \(/g;
     let aRegexResult = re.exec(new Error().stack);
     let sCallerName = aRegexResult[1] || aRegexResult[2];
-    alert("menu number is 12 and menu is called by: "+ sCallerName);
+    alert("menu Remove5reports is called by: "+ sCallerName);
 }
 
-function menu13() {
+function Add3rated() {
     let re = /([^(]+)@|at ([^(]+) \(/g;
     let aRegexResult = re.exec(new Error().stack);
     let sCallerName = aRegexResult[1] || aRegexResult[2];
-    alert("menu number is 13 and menu is called by: "+ sCallerName);
+    alert("menu Add3rated is called by: "+ sCallerName);
 }
 
-function menu15() {
+function Remove3rated() {
     let re = /([^(]+)@|at ([^(]+) \(/g;
     let aRegexResult = re.exec(new Error().stack);
     let sCallerName = aRegexResult[1] || aRegexResult[2];
-    alert("menu number is 15 and menu is called by: "+ sCallerName);
+    alert("menu Remove3rated is called by: "+ sCallerName);
 }
 
-function menu16() {
-    let re = /([^(]+)@|at ([^(]+) \(/g;
-    let aRegexResult = re.exec(new Error().stack);
-    let sCallerName = aRegexResult[1] || aRegexResult[2];
-    alert("menu number is 16 and menu is called by: "+ sCallerName);
-}
-
-function trackLocation() {
-    if (navigator.geolocation) {
-        // test to see if there is an active tracking and clear it if so 
-        // so that we don’t have multiple tracking going on
-        try { (navigator.geolocation.clearWatch(geoLocationID));}
-        catch (e){ console.log(e);}
-
-        // clear any existing data from the map
-        removeTracks();
-        // need to tell the tracker what we will do with the coordinates – showPosition
-        // also what we will do if there is an error – errorPosition
-        // also set some parameters – e.g how often to renew, what timeout to set
-        const options = { enableHighAccuracy: true, maximumAge: 30000, timeout: 27000};
-
-        geoLocationID = navigator.geolocation.watchPosition(showPosition, errorPosition, options);
-    } 
-    else {
-        document.getElementById('showLocation').innerHTML = "Geolocation is not supported by this browser."; 
-    }
-}
-
-
-function removePositionPoints() {
-    // disable the location tracking so that a new point won't be added while you are removing the old points 
-    // use the geoLocationID to do this
-    navigator.geolocation.clearWatch(geoLocationID);
-    removeTracks();
-    }
