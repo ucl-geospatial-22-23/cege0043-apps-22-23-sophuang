f657f616460678ad8a16dfb28bc0de88b203353c
sophuang <yiqi.huang.19@ucl.ac.uk>
2023-04-17 00:36:33 +0100

    Make the url of getting userRanking not hard coded

--- js/assetCreation.js
+++ js/assetCreation.js
@@ -26,7 +26,23 @@ function fetchUserId() {
   }
   
   
+ function fetchUserRanking(userId) {
+    let serviceUrl = document.location.origin + "/api/userRanking/" + userId;
   
+        $.ajax({
+            url: serviceUrl,
+            crossDomain: true,
+            type: "GET",
+            success: function(data) {
+            // Extract the ranking from the response data and show an alert
+            let ranking = data[0].array_to_json[0].rank;
+            alert("Your ranking based on condition reports is: " + ranking);
+            },
+            error: function(err) {
+            console.error("Error while fetching user ranking:", err);
+            }
+        });
+ } 
 
   
 ////////////////////////////////////////////
