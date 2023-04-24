7c141a8a7ca176a53281a5d4d3178c9ee515750f
sophuang <yiqi.huang.19@ucl.ac.uk>
2023-04-24 13:15:27 +0100

    Change the condition for the radar chart to be not hardcoded as well

--- js/radar_graph.js
+++ js/radar_graph.js
@@ -40,43 +40,57 @@ document.addEventListener("DOMContentLoaded", function() {
     fetchUserId()
       .then((userId) => {
         let serviceUrl = document.location.origin + "/api/userAssets/" + userId;
-        d3.json(serviceUrl)
-          .then((data) => {
-            data = preprocessData(data[0].features);
-            console.log(data);
+        d3.json(serviceUrl).then((rawData) => {
+
+            //data = preprocessData(data[0].features);
+            fetchConditionMapping()
+      .then(conditionMapping => {
+        // Fetch your data here
+        return { rawData, conditionMapping };
+      })
+      .then(({ rawData, conditionMapping }) => {
+        // Preprocess data using the fetched conditionMapping
+        rawData = rawData[0].features;
+        const data = preprocessData(rawData, conditionMapping);
   
-            // Prepare the radar chart data
-            let radarChartData = [
-                {
-                  className: "radar-data",
-                  axes: data.map((d) => {
-                    return {
-                      axis: d.name,
-                      value: d.condition_number,
-                      coordinates: d.geometry, // add this line
-                    };
-                  }),
-                },
-              ];
-            console.log(radarChartData);
+        // Continue with the rest of your code using the processedData
+        console.log(data);
+         // Prepare the radar chart data
+         let radarChartData = [
+          {
+            className: "radar-data",
+            axes: data.map((d) => {
+              return {
+                axis: d.name,
+                value: d.condition_number,
+                coordinates: d.geometry, // add this line
+              };
+            }),
+          },
+        ];
+      console.log(radarChartData);
 
-            // Get the container dimensions
-            const container = document.querySelector("#radarChartContainer");
-            const containerRect = container.getBoundingClientRect();
-            const containerWidth = containerRect.width;
-            const containerHeight = containerRect.height;
+      // Get the container dimensions
+      const container = document.querySelector("#radarChartContainer");
+      const containerRect = container.getBoundingClientRect();
+      const containerWidth = containerRect.width;
+      const containerHeight = containerRect.height;
 
 
 
-            // Create the radar chart
-            var chartConfig = {
-              w: containerWidth,
-              h: containerHeight-10,
-              maxValue: Math.max(...data.map(d => d.condition_number)),
-              levels: 5
-            };
+      // Create the radar chart
+      var chartConfig = {
+        w: containerWidth,
+        h: containerHeight,
+        maxValue: Math.max(...data.map(d => d.condition_number)),
+        levels: 5
+      };
+
+      RadarChart("#radarChartContainer", radarChartData, chartConfig);
+      });
+            
   
-            RadarChart("#radarChartContainer", radarChartData, chartConfig);
+           
           })
           .catch((error) => {
             console.error("Error fetching GeoJSON data:", error);
