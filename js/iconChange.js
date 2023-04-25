787fff8c02b0621ce168cb638da3342625289fc0
sophuang <yiqi.huang.19@ucl.ac.uk>
2023-04-25 19:22:32 +0100

    Added in-code comments for iconChnage.js

--- js/iconChange.js
+++ js/iconChange.js
@@ -1,10 +1,20 @@
+/* ////////////////////////////////////////////////////////////////////////////////////////
+
+This file sotres functions realting to change the color of marker according to the condtion
+
+The use of customized leaflet marker with different color is adapted from:
+https://github.com/pointhi/leaflet-color-markers
+
+*/ ////////////////////////////////////////////////////////////////////////////////////////
+
 "use strict";
 
-
+// This function create icon with different colors
+// using the reference from https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/
 function createCustomIcon(color) {
     return L.icon({
       iconUrl: `https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-${color}.png`,
-      shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+      shadowUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-shadow.png',
       iconSize: [25, 41],
       iconAnchor: [12, 41],
       popupAnchor: [1, -34],
@@ -12,16 +22,18 @@ function createCustomIcon(color) {
     });
   }
   
-  let greenIcon = createCustomIcon('green');
-  let yellowIcon = createCustomIcon('yellow');
-  let orangeIcon = createCustomIcon('orange');
-  let redIcon = createCustomIcon('red');
-  let violetIcon = createCustomIcon('violet');
-  let blueIcon = createCustomIcon('blue');
+// Get 6 different markers for later use
+let greenIcon = createCustomIcon('green');
+let yellowIcon = createCustomIcon('yellow');
+let orangeIcon = createCustomIcon('orange');
+let redIcon = createCustomIcon('red');
+let violetIcon = createCustomIcon('violet');
+let blueIcon = createCustomIcon('blue');  
   
 
-
-  function getIconByCondition(condition) {
+// return different icon by the condition pass into the function
+// this function can deal with condition_id value and condition_description both
+function getIconByCondition(condition) {
     let icon;
     switch (condition) {
       case '1':
@@ -44,58 +56,69 @@ function createCustomIcon(color) {
         case 'Not working and needs immediate, urgent maintenance':
         icon = violetIcon;
         break;
+      case '5':
+        case 'Unknown':
+        icon = violetIcon;
+        break;
       default:
         icon = blueIcon;
         break;
     }
     return icon;
-  }
+}
 
 
 
   
-
+// This function is used for update the color when a new condtion report is submitted
 function updateLayerColor(assetId, condition) {
-    let marker = markers[assetId];
-    updatedConditions[assetId] = condition;
-    if (marker) {
-        marker.remove();
-      let icon;
-      switch (condition) {
-        case '1':
-          icon = greenIcon;
-          break;
-        case '2':
-          icon = yellowIcon;
-          break;
-        case '3':
-          icon = orangeIcon;
-          break;
-        case '4':
-          icon = redIcon;
-          break;
-        case '5':
-          icon = violetIcon;
-          break;
-        default:
-          icon = blueIcon;
-          break;
-      }
-  
-      // Create a new marker with the updated icon and add it to the map
-    let newMarker = L.marker(marker.getLatLng(), {icon: icon}).addTo(mymap);
-    newMarker.bindPopup(marker.getPopup().getContent());
 
-    // Update the marker in the markers object
-    markers[assetId] = newMarker;
-    newMarker.assetId = assetId;
+  // Get the marker from the markers dictionary defined before
+  // by its asset_id 
+  let marker = markers[assetId];  
+
+  //Store the new condition into updateConditions dictionary
+  updatedConditions[assetId] = condition;
+  if (marker) {
+    marker.remove();
+    let icon;
+    switch (condition) {
+      case '1':
+        icon = greenIcon;
+        break;
+      case '2':
+        icon = yellowIcon;
+        break;
+      case '3':
+        icon = orangeIcon;
+        break;
+      case '4':
+        icon = redIcon;
+        break;
+      case '5':
+        icon = violetIcon;
+        break;
+      default:
+        icon = blueIcon;
+        break;
     }
   
-    console.log("updateLayerColor");
+  // Create a new marker with the updated icon and add it to the map
+  let newMarker = L.marker(marker.getLatLng(), {icon: icon}).addTo(mymap);
+  newMarker.bindPopup(marker.getPopup().getContent());
+
+  // Update the marker in the markers object
+  markers[assetId] = newMarker;
+  newMarker.assetId = assetId;
   }
+  
+  console.log("updateLayerColor");
+}
 
 
 
+// This function is used for updating the condition_desctiption result 
+// shown in the condition survey form, when the user click between different radio button
 function updateDescription(id) {
     let description = '';
     let descriptionDiv = document.getElementById('condition_description');
@@ -117,7 +140,7 @@ function updateDescription(id) {
         description = 'Not working and needs immediate, urgent maintenance';
         break;
       default:
-        description = '';
+        description = 'Unknown';
     }
     descriptionDiv.innerHTML = description;
 }
